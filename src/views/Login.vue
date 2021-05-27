6358484661f5d92c82c580b719507f079e1f9cec
Aleksey Medvedev <ctu4m@yandex.ru>
2021-05-27 20:12:50 +0300

    request work

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -98,11 +98,9 @@ export default {
     }
   },
   mounted(){
-    this.auth = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzc28uYnJlc3QuYXBwIiwic3ViIjoiYmI3OTYwODYtYmUwMS0xMWViLTgxY2ItNmVmOTFmZTZhODA1IiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjk5OTk5OTk5OTl9.86jG45MN1rsqqbysGTaMwrHyhxL4XDzY0_RLpWSLRgI";
+    this.auth = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzc28uYnJlc3QuYXBwIiwic3ViIjoiYmI3OTYwODYtYmUwMS0xMWViLTgxY2ItNmVmOTFmZTZhODA1IiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjk5OTk5OTk5OTl9.86jG45MN1rsqqbysGTaMwrHyhxL4XDzY0_RLpWSLRgI";
 
-    axios.get('https://api.brest.app/payments/',{
-
-    }, {
+    axios.get('https://api.brest.app/payment/',{
       headers:{
         Authorization: this.auth
       }
@@ -111,34 +109,34 @@ export default {
     });
 
 
-    axios.post("https://equilit.eu.auth0.com/oauth/token",{
-      "client_id":"hj8Ml5qdojTwwvMORVIK0TGvR98uDnXe",
-      "client_secret":"WyWsFEJwEFRwNNuedVdrQyFvdsVcYrA_AsKQknodmwQZb2RVvONMsCNwA-m_5fWd",
-      "audience":"https://equilit.eu.auth0.com/api/v2/",
-      "grant_type":"client_credentials"
-  }, {
-      headers: {
-        'content-type': 'application/json'
-      }
-    }).then((resp) =>{
-      let auth_app = resp.data.token_type + " " + resp.data.access_token;
-
-      // axios.post('https://equilit.eu.auth0.com/authorize',{
-      //   "response_type": "id_token token",
-      //   "redirect_uri": "http://admin.demo.tst/",
-      //   username: 'ctu4m@yandex.ru',
-      //   password: 'sfsfsfsf',
-      //   connection: 'Username-Password-Authentication',
-      //   "scope": "openid name"
-      // },{
-      //   headers: {
-      //     authorization : auth_app
-      //   }
-      // }).then((resp)=>{
-      //   console.log(resp)
-      // });
-
-    }).catch((error)=> console.log(error))
+  //   axios.post("https://equilit.eu.auth0.com/oauth/token",{
+  //     "client_id":"hj8Ml5qdojTwwvMORVIK0TGvR98uDnXe",
+  //     "client_secret":"WyWsFEJwEFRwNNuedVdrQyFvdsVcYrA_AsKQknodmwQZb2RVvONMsCNwA-m_5fWd",
+  //     "audience":"https://equilit.eu.auth0.com/api/v2/",
+  //     "grant_type":"client_credentials"
+  // }, {
+  //     headers: {
+  //       'content-type': 'application/json'
+  //     }
+  //   }).then((resp) =>{
+  //     // let auth_app = resp.data.token_type + " " + resp.data.access_token;
+  //
+  //     // axios.post('https://equilit.eu.auth0.com/authorize',{
+  //     //   "response_type": "id_token token",
+  //     //   "redirect_uri": "http://admin.demo.tst/",
+  //     //   username: 'ctu4m@yandex.ru',
+  //     //   password: 'sfsfsfsf',
+  //     //   connection: 'Username-Password-Authentication',
+  //     //   "scope": "openid name"
+  //     // },{
+  //     //   headers: {
+  //     //     authorization : auth_app
+  //     //   }
+  //     // }).then((resp)=>{
+  //     //   console.log(resp)
+  //     // });
+  //
+  //   }).catch((error)=> console.log(error))
   }
 };
 </script>
